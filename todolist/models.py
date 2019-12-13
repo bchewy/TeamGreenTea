37acc6a4728b74289b99682721e99e9906029814
Brian <brian@bchewy.com>
2019-12-14 00:40:41 +0800

    Fixed timestamps and replaced them with SG timezone

--- todolist/models.py
+++ todolist/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Category(models.Model):
@@ -14,10 +15,10 @@ class Item(models.Model):
 # TodoItem Model
 class TodoItem(models.Model):
     content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
 
 class TodoItemArchived(models.Model):
     content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
